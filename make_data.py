8eadc2eb8cc027e91b5965b6df2fc2082d9690e7
kthorbjornsen <kthorbjornsen@corvusenergy.com>
2025-03-27 13:37:38 +0100

    exension

--- make_data.py
+++ make_data.py
@@ -6,10 +6,7 @@ path = pathlib.Path(__file__).resolve().parent.joinpath("data")
 if not path.exists():
     path.mkdir()
 
-columns = columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+columns = columns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 for i in range(100):
-    df = pd.DataFrame(
-        np.random.randint(low=0, high=100, size=(int(1e4), 10)),
-        columns=columns
-    )
-    df.to_csv(path.joinpath(f"data_{i}"), index=False)
+    df = pd.DataFrame(np.random.randint(low=0, high=100, size=(int(1e4), 10)), columns=columns)
+    df.to_csv(path.joinpath(f"data_{i}.csv"), index=False)
